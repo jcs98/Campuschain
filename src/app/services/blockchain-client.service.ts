fca9d2336fe95c1383ee67a09033068c26e0775e
Jainam C Shah <jainamcshah98@gmail.com>
2020-03-22 12:15:25 +0530

    Define blockchain client functions

--- src/app/services/blockchain-client.service.ts
+++ src/app/services/blockchain-client.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { WalletService } from '../services/wallet.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainClientService {
 
-  constructor() { }
+  constructor(private walletService: WalletService) { }
+
+  addData(data) {
+    if (this.walletService.getPublicKey()) {
+      const sign = this.walletService.sign(data);
+      // create transaction and add to blockchain
+    } else {
+      alert('No keys found to sign transaction, please add a key pair first!');
+    }
+  }
+
+  verifyData(data, adderPublicKey) {
+    // get all transactions for adderPublicKey and verify if data exists in any of them
+  }
 }
